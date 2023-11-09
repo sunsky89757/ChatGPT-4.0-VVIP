8f8099b88d8a57834080d74e479289986b7d42db
sunsky89757 <113254524+sunsky89757@users.noreply.github.com>
2023-11-09 22:14:40 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -141,6 +141,10 @@ export const DEFAULT_MODELS = [
     name: "gpt-3.5-turbo-16k-0613",
     available: true,
   },
+  {
+  name: "DALLE-3",
+  available: true,
+  },
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
